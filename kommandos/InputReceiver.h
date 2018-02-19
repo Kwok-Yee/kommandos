452934dfd9485f15ca5c34e69a90f3c7c4bc6610
Commando <Kwok.Yee.Chu@hva.nl>
2018-02-19 14:19:57 +0100

    Added input for WASD, moving the sphere object in the scene

---
+++ kommandos/InputReceiver.h
@@ -0,0 +1,17 @@
+#pragma once
+
+class InputReceiver : public irr::IEventReceiver
+{
+public:
+	InputReceiver();
+
+	// This is the one method that we have to implement
+	virtual bool OnEvent(const irr::SEvent& event);
+
+	// This is used to check whether a key is being held down
+	virtual bool IsKeyDown(irr::EKEY_CODE keyCode) const;
+
+private:
+	// We use this array to store the current state of each key
+	bool KeyIsDown[irr::KEY_KEY_CODES_COUNT];
+};
--- kommandos/Main.cpp
+++ kommandos/Main.cpp
@@ -1,4 +1,6 @@
 #include <irrlicht.h>
+#include "driverChoice.h"
+#include "InputReceiver.h"
 
 using namespace irr;
 
@@ -16,45 +18,99 @@ using namespace gui;
 int main()
 {
 
-	IrrlichtDevice *device =
-		createDevice(video::EDT_SOFTWARE, dimension2d<u32>(800, 600), 16,
-			false, false, false, 0);
+	// Instance of inputReceiver
+	InputReceiver inputReceiver;
 
-	if (!device)
-		return 1;
+	// Create device
+	IrrlichtDevice* device = createDevice(video::EDT_DIRECT3D9,
+		core::dimension2d<u32>(800, 600), 16, false, false, false, &inputReceiver);
 
-	device->setWindowCaption(L"Hello World! - Irrlicht Engine Demo");
-	IVideoDriver* driver = device->getVideoDriver();
-	ISceneManager* smgr = device->getSceneManager();
-	IGUIEnvironment* guienv = device->getGUIEnvironment();
-
-	guienv->addStaticText(L"Hello World! This is the Irrlicht Software renderer!",
-		rect<s32>(10, 10, 260, 22), true);
-
-	IAnimatedMesh* mesh = smgr->getMesh("../media/sydney.md2");
-	if (!mesh)
-	{
-		device->drop();
+	// No device found
+	if (!device) {
 		return 1;
 	}
-	IAnimatedMeshSceneNode* node = smgr->addAnimatedMeshSceneNode(mesh);
 
-	if (node)
+	video::IVideoDriver* driver = device->getVideoDriver();
+	scene::ISceneManager* smgr = device->getSceneManager();
+
+	scene::ISceneNode * sphere = smgr->addSphereSceneNode();
+	if (sphere)
 	{
-		node->setMaterialFlag(EMF_LIGHTING, false);
-		node->setMD2Animation(scene::EMAT_STAND);
-		node->setMaterialTexture(0, driver->getTexture("../media/sydney.bmp"));
+		sphere->setPosition(core::vector3df(0, 0, 30));
+		sphere->setMaterialTexture(0, driver->getTexture("../media/wall.bmp"));
+		sphere->setMaterialFlag(video::EMF_LIGHTING, false);
 	}
-	smgr->addCameraSceneNode(0, vector3df(0, 30, -40), vector3df(0, 5, 0));
+	scene::ISceneNode* cube = smgr->addCubeSceneNode();
+
+	if (cube)
+	{
+		cube->setMaterialTexture(0, driver->getTexture("../media/t351sml.jpg"));
+		cube->setMaterialFlag(video::EMF_LIGHTING, false);
+		scene::ISceneNodeAnimator* anim =
+			smgr->createFlyCircleAnimator(core::vector3df(0, 0, 30), 20.0f);
+		if (anim)
+		{
+			cube->addAnimator(anim);
+			anim->drop();
+		}
+	}
+
+	smgr->addCameraSceneNodeFPS();
+	device->getCursorControl()->setVisible(false);
+
+	gui::IGUIStaticText* diagnostics = device->getGUIEnvironment()->addStaticText(
+		L"", core::rect<s32>(10, 10, 400, 20));
+	diagnostics->setOverrideColor(video::SColor(255, 255, 255, 0));
+
+	int lastFPS = -1;
+
+	// In order to do framerate independent movement, we have to know
+	// how long it was since the last frame
+	u32 then = device->getTimer()->getTime();
+
+	// This is the movemen speed in units per second.
+	const f32 MOVEMENT_SPEED = 5.f;
 
 	while (device->run())
 	{
-		driver->beginScene(true, true, SColor(255, 100, 101, 140));
+		// Work out a frame delta time.
+		const u32 now = device->getTimer()->getTime();
+		const f32 frameDeltaTime = (f32)(now - then) / 1000.f; // Time in seconds
+		then = now;
 
-		smgr->drawAll();
-		guienv->drawAll();
+		core::vector3df nodePosition = sphere->getPosition();
+
+		if (inputReceiver.IsKeyDown(irr::KEY_KEY_W))
+			nodePosition.Y += MOVEMENT_SPEED * frameDeltaTime;
+		else if (inputReceiver.IsKeyDown(irr::KEY_KEY_S))
+			nodePosition.Y -= MOVEMENT_SPEED * frameDeltaTime;
+
+		if (inputReceiver.IsKeyDown(irr::KEY_KEY_A))
+			nodePosition.X -= MOVEMENT_SPEED * frameDeltaTime;
+		else if (inputReceiver.IsKeyDown(irr::KEY_KEY_D))
+			nodePosition.X += MOVEMENT_SPEED * frameDeltaTime;
+
+		sphere->setPosition(nodePosition);
+
+		driver->beginScene(true, true, video::SColor(255, 113, 113, 133));
+
+		smgr->drawAll(); // draw the 3d scene
+		device->getGUIEnvironment()->drawAll(); // draw the gui environment (the logo)
 
 		driver->endScene();
+
+		int fps = driver->getFPS();
+
+		if (lastFPS != fps)
+		{
+			core::stringw tmp(L"Movement Example - Irrlicht Engine [");
+			tmp += driver->getName();
+			tmp += L"] fps: ";
+			tmp += fps;
+
+			device->setWindowCaption(tmp.c_str());
+			lastFPS = fps;
+		}
 	}
 	device->drop();
 

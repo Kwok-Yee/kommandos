e8de42450d7e1a06889caf739d27d19dde74a46c
Nino Thijs <ninothijs@gmail.com>
2018-04-25 15:36:30 +0200

    check for unique position.

--- kommandos/ObjectPlacementGeneration.cpp
+++ kommandos/ObjectPlacementGeneration.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <stdlib.h>
 #include <ctime>
+#include <algorithm>
 #include <irrlicht.h>
 
 using namespace irr;
@@ -14,20 +15,40 @@ void ObjectPlacementGeneration::PlaceObjects(IrrlichtDevice* device)
 	CreateGrid();
 	ISceneManager* smgr = device->getSceneManager();
 	core::array<ISceneNode*> obstacles;
+	vector3df* usedPositions;
 
 	srand(time(0));
 	int amountOfObjects = 10;
+	usedPositions = new vector3df[amountOfObjects];
 	for (int i = 0; i < amountOfObjects; i++)
 	{
 		obstacles.push_back(smgr->addCubeSceneNode());
-		obstacles[i]->setPosition(grid[randomPosition()]);
+		usedPositions[i] = grid[randomPosition()];
+		obstacles[i]->setPosition(usedPositions[i]);
+
+	}
+
+	int count = 0;
+	for (int i = 0; i < amountOfObjects - 1; i++)
+	{
+		for (int j = i + 1; j < amountOfObjects; j++)
+		{
+			if (usedPositions[i] == usedPositions[j]) {
+				printf("not unique");
+			}
+		}
 	}
 }
 
 
 ObjectPlacementGeneration::ObjectPlacementGeneration()
 {
-	
+
+}
+
+void ObjectPlacementGeneration::AddObjectsToAvoid(ISceneNode* object)
+{
+	objectsToAvoid.push_back(object);
 }
 
 void ObjectPlacementGeneration::CreateGrid()
@@ -58,11 +79,13 @@ void ObjectPlacementGeneration::CalculateGrid(ISceneNode* arena, ISceneNode* obs
 	vector3df arenaSize = arena->getTransformedBoundingBox().getExtent();
 	vector3df obstacleSize = obstacle->getBoundingBox().getExtent();
 
-	rows = arenaSize.X / obstacleSize.X;
-	columns = arenaSize.Z / obstacleSize.Z;
+	f32 scale = 0.9;
 
-	startX = arenaSize.X / 2 - obstacleSize.X;
-	startZ = arenaSize.Z / 2 - obstacleSize.Z;
+	rows = (arenaSize.X * scale) / obstacleSize.X;
+	columns = (arenaSize.Z * scale) / obstacleSize.Z;
+
+	startX = (arenaSize.X * scale) / 2 - obstacleSize.X;
+	startZ = (arenaSize.Z * scale) / 2 - obstacleSize.Z;
 
 	grid = new vector3df[rows * columns];
 }
--- kommandos/ObjectPlacementGeneration.h
+++ kommandos/ObjectPlacementGeneration.h
@@ -4,16 +4,19 @@
 class ObjectPlacementGeneration
 {
 public:
+	ObjectPlacementGeneration();
+
 	void PlaceObjects(irr::IrrlichtDevice* device);
 	void CalculateGrid(irr::scene::ISceneNode* arena, irr::scene::ISceneNode* obstacle);
-	ObjectPlacementGeneration();
-	irr::core::array<vector3df> positionsToAvoid;
+	void AddObjectsToAvoid(irr::scene::ISceneNode* object);
+
 private:
 	int rows;
 	int columns;
 	int startX, startZ;
 	irr::core::vector3df *grid;
-	
+	irr::core::array<irr::scene::ISceneNode*> objectsToAvoid;
+
 	void CreateGrid();
 	int randomPosition();
 	

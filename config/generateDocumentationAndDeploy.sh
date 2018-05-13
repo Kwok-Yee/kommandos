1a0c4ec366b0dd857ee5168749d0dfb869ff72bb
Kwok Yee Chu <k.y.chu96@gmail.com>
2018-05-13 14:49:24 +0200

    correct path to doxyfile

--- config/generateDocumentationAndDeploy.sh
+++ config/generateDocumentationAndDeploy.sh
@@ -18,7 +18,7 @@ echo "" > .nojekyll
 
 echo 'Generating Doxygen code documentation...'
 
-doxygen $DOXYFILE 2>&1 | tee doxygen.log
+doxygen kommandos/$DOXYFILE 2>&1 | tee doxygen.log
 
 if [ -d "html" ] && [ -f "html/index.html" ]; then
 

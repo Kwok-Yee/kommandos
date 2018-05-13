e95f17e90e13a298ff0ce9b21b8097e54b769722
Kwok Yee Chu <k.y.chu96@gmail.com>
2018-05-13 18:04:38 +0200

    added shell

---
+++ kommandos/generateDocumentationAndDeploy.sh
@@ -0,0 +1,37 @@
+echo 'Setting up the script...'
+
+set -e
+
+mkdir code_docs
+cd code_docs
+
+git clone -b gh-pages https://github.com/${TRAVIS_REPO_SLUG}
+cd ${TRAVIS_REPO_SLUG##*/}
+
+git config --global push.default simple
+git config user.name "Travis CI"
+git config user.email "travis@travis-ci.org"
+
+rm -rf *
+
+echo "" > .nojekyll
+
+echo 'Generating Doxygen code documentation...'
+
+doxygen $DOXYFILE 2>&1 | tee doxygen.log
+
+if [ -d "docs/html" ] && [ -f "docs/html/index.html" ]; then
+
+    echo 'Uploading documentation to the gh-pages branch...'
+
+    git add --all
+
+    git commit -m "Deploy code docs to GitHub Pages Travis build: ${TRAVIS_BUILD_NUMBER}" -m "Commit: ${TRAVIS_COMMIT}"
+
+    git push --force "https://${GH_REPO_TOKEN}@github.com/${TRAVIS_REPO_SLUG}"
+else
+    echo '' >&2
+    echo 'Warning: No documentation (html) files have been found!' >&2
+    echo 'Warning: Not going to push the documentation to GitHub!' >&2
+    exit 1
+fi

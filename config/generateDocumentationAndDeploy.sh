df88250baa98b42ae3e06c850b86cb015f08adbe
Kwok Yee Chu <k.y.chu96@gmail.com>
2018-05-13 17:00:26 +0200

    add update to addons

--- config/generateDocumentationAndDeploy.sh
+++ config/generateDocumentationAndDeploy.sh
@@ -12,8 +12,7 @@ git config --global push.default simple
 git config user.name "Travis CI"
 git config user.email "travis@travis-ci.org"
 
-shopt -s extglob dotglob
-rm -rf !(index.html)
+rm -rf *
 
 echo "" > .nojekyll
 

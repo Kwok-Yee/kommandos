af5b24baa2f3c0c18a993e2a4c48f97233bae95b
Aness Ahmad <anessahmad20@gmail.com>
2018-05-14 13:54:53 +0200

    refactor score script added comments and change statictext with draw [WIP] scale text with screen

--- kommandos/Score.cpp
+++ kommandos/Score.cpp
@@ -7,35 +7,42 @@ using namespace core;
 using namespace scene;
 using namespace video;
 
-IrrlichtDevice* scoreIDevice;
 IGUIEnvironment* scoreGui;
-IVideoDriver* scoreDriver;
-ISceneManager* scoreSmgr;
+IGUIFont* font2;
+u32 timer;
 
-s32 score = 10;
+EGUI_ALIGNMENT upperleft,upperright;
+
+
+s32 score = 0;
 IGUIStaticText* scoreText;
 
-Score::Score()
+
+void Score::Scoring(IrrlichtDevice* device) 
 {
-}
-
-void Score::Scoring(IrrlichtDevice* device) {
-	scoreIDevice = device;
-	scoreDriver = device->getVideoDriver();
 	scoreGui = device->getGUIEnvironment();
-	scoreSmgr = device->getSceneManager();
 
-	//score += points;
-	scoreText = scoreGui->addStaticText(stringw(score).c_str(),
-			rect<s32>(80, 80, 200, 100), true);
+	//scoreText = scoreGui->addStaticText(stringw(score).c_str(),
+	//		rect<s32>(80, 80, 200, 100), true);
+
+	timer = device->getTimer()->getTime();
+
+	font2 = device->getGUIEnvironment()->getFont("../media/fontlucida.png");
+	font2->draw(stringw(score).c_str(),
+		core::rect<s32>(80, 80, 200, 100),
+		video::SColor(255, 255, 255, 255));
+	
+	//core::rect<s32>(80, 50, 200, 100)
+
+
 	DisplayScore(0);
 
 }
 
-void Score::DisplayScore(f32 points) {
-
+//updates the score of the player 
+void Score::DisplayScore(f32 points) 
+{
 	score += points;
-	scoreText->setText(stringw(score).c_str());
-	
+	//scoreText->setText(stringw(score).c_str());
 	
 }

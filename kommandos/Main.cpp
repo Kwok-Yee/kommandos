4c3e62d3dbf6eac3c042a6ffe37829aeda156769
Commando <Kwok.Yee.Chu@hva.nl>
2018-05-14 14:54:08 +0200

    add nicer comment

--- kommandos/Main.cpp
+++ kommandos/Main.cpp
@@ -2,11 +2,10 @@
 *
 * \section intro_sec Introduction
 *
-* Top-down arcade shooter featuring waves of enemies in procedurally generated levels
-* For our main technical goals, we chose to implement: Procedural generation, performance optimization, and controller support
+* Top-down arcade shooter featuring waves of enemies in procedurally generated levels.
+* For our main technical goals, we chose to implement: Procedural generation, performance optimization, and controller support.
 * This would work perfectly in our game because we could randomly generate the layout of each level, 
-* while watching performance with the massive hordes of zombies that will be spawned, 
-* and allowing use of a ps4 controller for satisfying ease of use. 
+* while watching performance with the massive hordes of zombies that will be spawned, the player uses WASD and his mouse for movement and shooting.
 * The scope for our game is reachable by our team, while maintaining creative possibilities.
 *
 * \section install_sec Installation

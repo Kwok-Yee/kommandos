fe8c3545e6263cb77ef0b6e0c735f6178a6a195a
Timo Bos <timo.bos2@hva.nl>
2018-05-16 13:23:28 +0200

    Comments for enemy behaviour

--- kommandos/EnemyBehaviour.cpp
+++ kommandos/EnemyBehaviour.cpp
@@ -1,52 +1,42 @@
 #include <irrlicht.h>
 #include "EnemyBehaviour.h"
 #include "Collision.h"
-#include "Player.h"
-#include "Score.h"
-
 
 using namespace irr;
 using namespace core;
 using namespace scene;
 using namespace video;
 
-// This is the movement speed in units per second.
-const f32 ENEMY_MOVEMENT_SPEED = 15.f;
-const s32 ENEMY_MAX_HEALTH = 100;
-
-//Player *player;
+// Movement speed in units per second.
+#define ENEMY_MOVEMENT_SPEED 15.f;
+const vector3df scaleVect = vector3df(2.0f, 2.0f, 2.0f);
+const f32 meleeRange = vector3df(4, 4, 4).getLength();
 
 IrrlichtDevice* enemyBehaviourIDevice;
-IVideoDriver* enemyBevaiourDriver;
-ISceneManager* enemyBaviourSmgr;
-f32 enemyHealth;
-
+IVideoDriver* enemyBehaviourDriver;
+ISceneManager* enemyBehaviourSmgr;
 IMeshSceneNode* enemyNode;
-Score _score;
-
-
+Collision col;
 
 EnemyBehaviour::EnemyBehaviour(IrrlichtDevice* device) {
 	enemyBehaviourIDevice = device;
-	enemyBevaiourDriver = enemyBehaviourIDevice->getVideoDriver();
-	enemyBaviourSmgr = enemyBehaviourIDevice->getSceneManager();
+	enemyBehaviourDriver = enemyBehaviourIDevice->getVideoDriver();
+	enemyBehaviourSmgr = enemyBehaviourIDevice->getSceneManager();
 }
 
 IMeshSceneNode* EnemyBehaviour::Spawn(vector3df startPosition)
 {
-	enemyHealth = ENEMY_MAX_HEALTH;
-	// spawn enemy
-	enemyNode = enemyBaviourSmgr->addMeshSceneNode(enemyBaviourSmgr->getMesh("../media/ninja.b3d"));
+	enemyNode = enemyBehaviourSmgr->addMeshSceneNode(enemyBehaviourSmgr->getMesh("../media/ninja.b3d"));
 	if (enemyNode)
 	{
 		enemyNode->setMaterialFlag(video::EMF_LIGHTING, false);
-		enemyNode->setScale(vector3df(10, 10, 10));
-		/*enemyNode->setFrameLoop(0, 13); // For walking animation
-		enemyNode->setAnimationSpeed(15);*/
+		// For walking animation
+		//enemyNode->setFrameLoop(0, 13); 
+		//enemyNode->setAnimationSpeed(15);
 
-		enemyNode->setScale(vector3df(2.f, 2.f, 2.f));
+		enemyNode->setScale(scaleVect);
 		enemyNode->setRotation(vector3df(0, -90, 0));
-		enemyNode->setMaterialTexture(0, enemyBevaiourDriver->getTexture("../media/nskinrd.jpg"));
+		enemyNode->setMaterialTexture(0, enemyBehaviourDriver->getTexture("../media/nskinrd.jpg"));
 		enemyNode->setPosition(startPosition);
 		return enemyNode;
 	}
@@ -62,37 +52,34 @@ bool EnemyBehaviour::Move(IMeshSceneNode* enemyNode, vector3df playerPosition, f
 {
 	// Get position delta compared to player position
 	vector3df enemyPosition = enemyNode->getPosition();
-	vector3df delta = playerPosition - enemyPosition; //Save delta
+	vector3df delta = playerPosition - enemyPosition; // Save delta
 	vector3df deltaNormalized = delta;
-	deltaNormalized.normalize(); // If it isnt done in two lines, the delta gets normalized
+	deltaNormalized.normalize(); // It is done in two lines, so the delta doesnt get normalized
 
-	enemyNode->setRotation(core::vector3df(0, atan2(deltaNormalized.X, deltaNormalized.Z) * 180 / PI, 0));
+	enemyNode->setRotation(vector3df(0, atan2(deltaNormalized.X, deltaNormalized.Z) * 180 / PI, 0));
 
 	// Change position based on delta and speed
-	if (delta.getLength() > vector3df(4, 4, 4).getLength())
+	if (delta.getLength() > meleeRange) // If farther than melee attack range,
 	{
-		enemyPosition += deltaNormalized * ENEMY_MOVEMENT_SPEED * frameDeltaTime;
+		// Move towards player
+		enemyPosition += deltaNormalized * frameDeltaTime * ENEMY_MOVEMENT_SPEED;
 		vector3df oldPosition = enemyNode->getPosition();
 		enemyNode->setPosition(enemyPosition);
 
-		Collision collision;
-		if (collision.CollidesWithStaticObjects(enemyNode))
+		// Confirm static object collision
+		if (col.CollidesWithStaticObjects(enemyNode))
 			enemyNode->setPosition(oldPosition);
 		return false;
-
 	}
 	else
 		return true;
 }
 
-
-
-f32 EnemyBehaviour::TakeDamage(f32 damage, f32 health) 
+f32 EnemyBehaviour::TakeDamage(f32 damage, f32 health)
 {
 	if (health > 0) {
 		health -= damage;
 	}
-
+	else health = 0;
 	return health;
-
 }

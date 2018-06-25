26071acd0e5f48f981d760c8a76e9389b5847f08
ninothijs <ninothijs@gmail.com>
2018-06-25 16:20:06 +0200

    Added perlin noise script for now.

---
+++ kommandos/PerlinNoise.h
@@ -0,0 +1,18 @@
+#include <irrlicht.h>
+#include <vector>
+#pragma once
+
+class PerlinNoise
+{
+	std::vector<int> p;
+public:
+	
+	PerlinNoise();
+	PerlinNoise(unsigned int seed);
+	double noise(double x, double y, double z);
+private:
+	double fade(double t);
+	double lerp(double t, double a, double b);
+	double grad(int hash, double x, double y, double z);
+};
+

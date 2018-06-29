dfd90907766a4b9e0fbd39c2450ae1076cf709fa
Lazysparks <thomasballais@gmail.com>
2018-06-29 23:30:10 +0200

    Enemy Wave Manager implemented

--- kommandos/EnemySpawner.cpp
+++ kommandos/EnemySpawner.cpp
@@ -16,7 +16,7 @@ using namespace core;
 using namespace scene;
 using namespace std;
 
-const u32 maxWaves = 10;
+const u32 maxWaves = 11;
 
 IrrlichtDevice* enemySpawnerIDevice;
 ISceneManager* enemySpawnerSmgr;
@@ -28,7 +28,7 @@ Collision collision;
 
 core::array<Enemy*> activeEnemies;
 irr::core::array<vector3df> spawnPositions;
-u32 amountOfEnemies, resize;
+u32 resize;
 u32 currentWave = 0;
 
 ParticleSystem *particle;
@@ -44,7 +44,6 @@ EnemySpawner::EnemySpawner(IrrlichtDevice* device, Player* Player)
 	game_EnemySpawner = game_EnemySpawner->GetInstance();
 	enemyPool = EnemyPool::GetInstance(device);
 
-	amountOfEnemies = 12;
 	resize = 2;
 	//setting spawnpositions in the corners.
 	spawnPositions.push_back(vector3df(-82, 0, -78) * resize);
@@ -56,26 +55,26 @@ EnemySpawner::EnemySpawner(IrrlichtDevice* device, Player* Player)
 	// In order to do framerate independent movement, we have to know
 	// how long it was since the last frame
 	prevFrameTime = enemySpawnerIDevice->getTimer()->getTime();
-
+	InitialiseWaveData();
 	Spawn();
 }
 //Struct for wavedata for each enemy in wave
 struct WaveData
 {
-	 WaveData(int _waveNumber, int _maxRegularEnem, int _maxFastEnem, int _maxTankEnem)
+	WaveData(int _waveNumber, int _maxRegularEnem, int _maxFastEnem, int _maxTankEnem)
 	{
 		waveNumber = _waveNumber;
 		maxRegularEnem = _maxRegularEnem;
 		maxFastEnem = _maxFastEnem;
 		maxTankEnem = _maxTankEnem;
-		
-	}
+
+	};
 	int waveNumber;
 	int maxRegularEnem;
 	int maxFastEnem;
 	int maxTankEnem;
 };
-WaveData waveData[2];
+WaveData* waveData[15];
 
 void EnemySpawner::UpdateEnemies()
 {
@@ -106,56 +105,72 @@ void EnemySpawner::UpdateEnemies()
 
 	if (activeEnemies.size() <= 0 && currentWave < maxWaves)
 	{
-		Spawn();
 		currentWave++;
+		Spawn();
+		//Insert pause/UI between waves here
+		printf("wave changed");
 	}
 }
 
 
 void EnemySpawner::InitialiseWaveData()
 {
-										//Amount of enemies:
-	waveData[0] = WaveData(1, 5, 0, 0); //5
-	waveData[1] = WaveData(2, 10, 0, 0);//10
-	waveData[2] = WaveData(3, 8, 3, 0);//11
-	waveData[3] = WaveData(4, 12, 5, 0);//17
-	waveData[4] = WaveData(5, 15, 2, 2);//19
-	waveData[5] = WaveData(6, 10, 0, 7);//17
-	waveData[6] = WaveData(7, 10, 0, 0);//Rest will change depending on yet to be added enemy types
-	waveData[7] = WaveData(8, 10, 0, 0);
-	waveData[8] = WaveData(9, 10, 0, 0);
-	waveData[9] = WaveData(10, 30, 0, 0);
-	waveData[10] = WaveData(11, 10, 0, 0);
-
+				        //Amount of enemies:
+	waveData[0] = new WaveData(1, 1, 0, 0);//10
+	waveData[1] = new WaveData(2, 0, 0, 10);//10
+	waveData[2] = new WaveData(3, 8, 3, 0);//11
+	waveData[3] = new WaveData(4, 12, 5, 0);//17
+	waveData[4] = new WaveData(5, 15, 2, 2);//19
+	waveData[5] = new WaveData(6, 10, 0, 7);//17
+	waveData[6] = new WaveData(7, 10, 0, 0);//Rest will change depending on yet to be added enemy types
+	waveData[7] = new WaveData(8, 10, 0, 0);
+	waveData[8] = new WaveData(9, 10, 0, 0);
+	waveData[9] = new WaveData(10, 30, 0, 0);
+	waveData[10] = new WaveData(11, 10, 0, 0);
 }
 
 void EnemySpawner::Spawn()
 {
-	for (int i = 0; i < amountOfEnemies; i++)
+	for (int i = 0; i < waveData[currentWave]->maxRegularEnem; i++)
 	{
 		srand(time(NULL) * i);
 		u32 randomPos = rand() % 4;
-		u32 enemyType = rand() % 3;
 
 		enemy = enemyPool->GetResource();
 		enemy->SetPlayer(_player);
-		switch (enemyType) {
-		case 0:
-			enemy->SetEnemyType(Enemy::EnemyType::basic);
-			break;
-		case 1:
-			enemy->SetEnemyType(Enemy::EnemyType::fast);
-			break;
-		case 2:
-			enemy->SetEnemyType(Enemy::EnemyType::tanky);
-			break;
-		}
+		enemy->SetEnemyType(Enemy::EnemyType::basic);
+		enemy->GetEnemySceneNode()->setPosition(spawnPositions[randomPos]);
+		collision.AddDynamicToList(enemy->GetEnemySceneNode());
+		activeEnemies.push_back(enemy);
+	}
 
+	for (int j = 0; j < waveData[currentWave]->maxFastEnem; j++)
+	{
+		srand(time(NULL) * j);
+		u32 randomPos = rand() % 4;
+
+		enemy = enemyPool->GetResource();
+		enemy->SetPlayer(_player);
+		enemy->SetEnemyType(Enemy::EnemyType::fast);
+		enemy->GetEnemySceneNode()->setPosition(spawnPositions[randomPos]);
+		collision.AddDynamicToList(enemy->GetEnemySceneNode());
+		activeEnemies.push_back(enemy);
+	}
+
+	for (int k = 0; k < waveData[currentWave]->maxTankEnem; k++)
+	{
+		srand(time(NULL) * k);
+		u32 randomPos = rand() % 4;
+
+		enemy = enemyPool->GetResource();
+		enemy->SetPlayer(_player);
+		enemy->SetEnemyType(Enemy::EnemyType::tanky);
 		enemy->GetEnemySceneNode()->setPosition(spawnPositions[randomPos]);
 		collision.AddDynamicToList(enemy->GetEnemySceneNode());
 		activeEnemies.push_back(enemy);
 	}
 }
+
 
 core::array<Enemy*> EnemySpawner::getActiveEnemies() { return activeEnemies; }
 Enemy* EnemySpawner::GetEnemy(int id) { return activeEnemies[id]; }
